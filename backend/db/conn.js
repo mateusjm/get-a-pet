3a781b0bf5cc2e34a0b836cc13b0fcc7d35e7026
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-17 14:41:05 -0300

    criando a conexão entre o banco de dados e o projeto com Mongoose

---
+++ backend/db/conn.js
@@ -0,0 +1,14 @@
+// importando o Mongoose
+const mongoose = require('mongoose')
+
+// definindo função main e criando banco GetAPet
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/getapet')
+    console.log('Conectou ao Mongoose')
+}
+
+// ativando função e verificando possível erro
+main().catch((err)=> console.log(err))
+
+// exportando mongoose
+module.exports = mongoose
