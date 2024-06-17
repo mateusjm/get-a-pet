55c0793f9f31e411a9009adc2c958c8cae935bbe
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-17 20:00:47 -0300

    rota register
    criando funcionalidades, formas de validar o usuário
    checar se já há um e-mail existente

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,8 +4,51 @@ const User = require('../models/User')
 // exportando UserController
 module.exports = class UserController {
 
+    // registro
     static async register(req, res) {
-        res.json({message: 'Olá Get A Pet!'})
-    }
 
+        // puxando os dados do body pela requisição
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        // validações para as requisições
+        if(!name) {
+            res.status(422).json({message: 'O nome é obrigatório!'})
+            return
+        }
+
+        if(!email) {
+            res.status(422).json({message: 'O e-mail é obrigatório!'})
+            return
+        }
+
+        if(!phone) {
+            res.status(422).json({message: 'O telefone é obrigatório!'})
+            return
+        }
+
+        if(!password) {
+            res.status(422).json({message: 'A senha é obrigatória!'})
+            return
+        }
+
+        if(!confirmpassword) {
+            res.status(422).json({message: 'A confirmação de senha é obrigatória!'})
+            return
+        }
+
+        // validação de senha e confirmação de senha
+        if(password !== confirmpassword) {
+            res.status(422).json({message: 'As senhas não conferem!'})
+            return
+        }
+
+        // checando se usuário já existe, email que veio pelo body
+        const userExists = await User.findOne({email: email})
+
+        // se o email que vir da requisição for encontrado no atributo email, enviamos erro
+        if(userExists) {
+            res.status(422).json({message: 'Por favor, utilize outro e=mail!'})
+            return
+        }
+    }
 }
