f4b3371580463dee6523e49f96c9da204bdb347f
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-18 14:52:22 -0300

    inserindo login nas rotas do UserController

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -6,6 +6,7 @@ const UserController = require('../controllers/UserController')
 
 // adicionando rotas a UserController pelas funções
 router.post('/register', UserController.register)
+router.post('/login', UserController.login)
 
 // exportando Router
 module.exports = router
