5b5212e6f12bae99a92f4d5b6efa2a11692acb19
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-28 19:34:36 -0300

    adicionando rotas
    checkUser, getUserByID e editUser com a função de ajuda verifyToken

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,9 +4,15 @@ const router = require('express').Router()
 // importando User Controller
 const UserController = require('../controllers/UserController')
 
+// midlleware
+const verifyToken = require('../helpers/verify-token')
+
 // adicionando rotas a UserController pelas funções
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.get('/checkuser', UserController.checkUser)
+router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 // exportando Router
 module.exports = router
