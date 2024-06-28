4b900097eab424c1e9ee015c9c0617e1d9983cdb
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-28 19:38:44 -0300

    validação do token do usuário
    jwt verifica a autenticidade do token, se ele condiz ele prossegue
    caso não seja o token, o usuário recebe a mensagem

---
+++ backend/helpers/verify-token.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const getToken = require('./get-token')
+
+// validação do token do usuário
+const checkToken = (req, res, next) => {
+
+    // caso usuário não receba o headers
+    if(!req.headers.authorization) {
+        return res.status(401).json({message: 'Acesso negado!'})
+    }
+
+    // obtendo o token
+    const token = getToken(req)
+    
+    // caso usuário não tenha o token
+    if(!token) {
+        return res.status(401).json({message: 'Acesso negado!'})
+    }
+
+    // jwt vai verificar a autencidade do token, se ele condiz 
+    try {
+        const verified = jwt.verify(token, 'nossosecret')
+        req.user = verified
+        next()
+
+    } catch(err) {
+        return res.status(400).json({message: 'Token inválido!'})
+    }
+
+}
+
+module.exports = checkToken
+
