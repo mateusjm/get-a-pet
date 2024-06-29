773b7052eb5dda0af55fb592df5c57c2d152beec
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-28 21:55:20 -0300

    adicionando rota de edição do usuário e upload de imagem

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -6,13 +6,14 @@ const UserController = require('../controllers/UserController')
 
 // midlleware
 const verifyToken = require('../helpers/verify-token')
+const {imageUpload} = require('../helpers/image-upload')
 
 // adicionando rotas a UserController pelas funções
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
+router.patch('/edit/:id', verifyToken, imageUpload.single('image') ,UserController.editUser)
 
 // exportando Router
 module.exports = router
