8cdf1bea05e2d44656f986693806d1894238e00e
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-07-15 21:54:06 -0300

    adicionando react no projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+// importando itens do React Router DOM
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+/* Components */
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+
+/* Pages */
+import Login from './components/pages/auth/Login'
+import Register from './components/pages/auth/Register'
+import Home from './components/pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
-  );
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
-export default App;
+export default App
